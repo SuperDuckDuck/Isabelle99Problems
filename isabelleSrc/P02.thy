fe7ce675ecd04c920ee686fcde682b3f79a9e2ae
SuperDuckDuck <SvenWille@users.noreply.github.com>
2017-09-16 20:03:32 +0200

    added solution p02_2 and p03_1

--- isabelleSrc/P02.thy
+++ isabelleSrc/P02.thy
@@ -88,9 +88,55 @@ proof -
   thus "\<forall> xs. p02_1 xs defa = defa \<or> p02_1 xs defa \<in> set xs" by auto
 qed
   
- 
+lemma "length xs \<ge> 2 \<Longrightarrow> p02_1 xs defa = xs ! ((length xs) -2) " by (induct xs defa rule : p02_1.induct, simp_all)
+    
+lemma "length ys = 2 \<Longrightarrow> p02_1 (xs @ ys) defa = hd ys" by  (induct xs defa rule : p02_1.induct; induct ys defa rule : p02_1.induct, simp_all)
+
 fun p02_2 :: "'a list \<Rightarrow> 'a option" where 
 "p02_2 [] = None"|
 "p02_2 [x] = None"|
 "p02_2 [x,y] = Some x"|
 "p02_2 (x#xs) = p02_2 xs"
+
+
+value "p02_2 [] :: 'a option"
+  
+value "p02_2 [1] :: int option"
+  
+value "p02_2 [1,2] :: int option"
+  
+lemma "p02_2 [] = None" by simp
+    
+lemma "p02_2 [val] = None" by simp
+    
+lemma "p02_2 [val1,val2] = Some val1" by simp
+    
+lemma "p02_2 (xs @ [val1 , val2]) = Some val1" by (induct xs rule : p02_2.induct, simp_all)
+
+lemma "length xs \<ge> 2 \<Longrightarrow> \<exists>x . p02_2 xs = Some x" by (induct xs rule : p02_2.induct, simp_all)    
+    
+lemma "length xs \<ge> 2 \<Longrightarrow> the (p02_2 xs)  \<in> set xs" by (induct xs rule : p02_2.induct, simp_all)
+    
+lemma "length xs < 2 \<Longrightarrow> p02_2 xs = None" by (induct xs rule : p02_2.induct, simp_all)    
+find_theorems "\<not>(\<forall>x . ?P x)" 
+  
+lemma "\<forall> xs:: 'a list . (p02_2 xs  = None) \<or> (the (p02_2 xs)  \<in> set xs) " 
+proof -
+  {
+    assume "\<not>(\<forall> xs:: 'a list . p02_2 xs  = None \<or> (the (p02_2 xs)  \<in> set xs ))" 
+    hence a:"\<exists> xs . ((p02_2 (xs :: 'a list)  \<noteq> None) \<and> (the (p02_2 xs)  \<notin> set xs)) " by simp 
+    {
+      fix as
+      assume "(p02_2 (as :: 'a list)  \<noteq> None) \<and> (the (p02_2 as)  \<notin> set as)" 
+      hence False by (induct as rule : p02_2.induct, simp_all)
+    }
+    with a have False by (rule exE)     
+  }
+  thus ?thesis by blast   
+qed
+    
+lemma "length ys = 2 \<Longrightarrow> the (p02_2 (xs @ ys)) = hd ys" by (induct xs rule : p02_2.induct; induct ys rule : p02_2.induct, simp_all)  
+  
+lemma "length xs \<ge> 2 \<Longrightarrow> the (p02_2 xs) = xs ! ((length xs) - 2)" by (induct xs rule : p02_2.induct, simp_all)
+  
+lemma "length xs \<ge> 2 \<Longrightarrow> the (p02_2 xs) = p02_1 xs defa" by (induct xs rule : p02_2.induct, simp_all) 
--- isabelleSrc/P03.thy
+++ isabelleSrc/P03.thy
@@ -1,7 +1,7 @@
 theory P03
 imports Main 
 begin 
-
+(*
 fun p03_simple :: "'a list \<Rightarrow> nat \<Rightarrow> 'a option" where 
   "p03_simple [] _ = None"|
   "p03_simple (x#xs) n = (if n = 0 then Some x else p03_simple xs (n - 1))"
@@ -105,11 +105,157 @@ qed
   qed
     
 qed
+*)  
   
+primrec p03_1 ::  "'a list \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a" where
+  "p03_1 [] _ defa = defa"|
+  "p03_1 (x#xs) n defa = (case n of 0 \<Rightarrow> defa | (Suc 0) \<Rightarrow> x  | _ \<Rightarrow> p03_1 xs (n -1) defa)"
   
+value "p03_1 [] 3 5 :: int"
   
+value "p03_1 [1] 1 5 :: int"
   
+value "p03_1 [1,2,3,4] 3 5:: int"
+  
+value "p03_1 [1] 0 5 :: int"
+  
+value "p03_1 [] 0 5 :: int"
+  
+lemma "p03_1 [] n defa = defa" by simp
+    
+lemma "p03_1 xs 0 defa = defa" by (induct xs , simp_all)
+    
+lemma "n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> p03_1 ls n defa \<in> set ls"
+proof -
+  assume hyp1:"n \<le> length ls"
+     and hyp2:"n \<noteq> 0"
+  thus "p03_1 ls n defa \<in> set ls " 
+  proof (induct ls arbitrary : n)
+    case Nil
+    then show ?case by simp
+  next
+    case (Cons a ls)
+    assume hypa1:"\<And>n . n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> p03_1 ls n defa \<in> set ls"
+      and hypa2:"n \<le> length (a # ls)"
+      and hypa3:"n \<noteq> 0"
+    from hypa2 and hypa3  show ?case 
+    proof (cases n)
+      case 0
+      with hypa3 show ?thesis by simp
+    next
+      case (Suc nat)
+      assume nHyp:"n = Suc nat"
+      then show ?thesis 
+      proof (cases nat)
+        case 0
+        with nHyp show ?thesis by simp
+      next
+        case (Suc nat2)
+        assume n2Hyp:"nat = Suc nat2"
+        with nHyp have tmp:"n = Suc (Suc nat2)" by simp
+        with hypa1 and hypa2 and hypa3 show ?thesis by simp
+      qed
+    qed
+  qed
+qed
+  
+lemma "n > length ls \<Longrightarrow> p03_1 ls n defa = defa" 
+proof (induct ls arbitrary : n)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons a ls)
+  assume hyp1:"\<And>n. length ls < n \<Longrightarrow> p03_1 ls n defa = defa"
+  assume hyp2:"length (a # ls) < n"
+  thus ?case 
+  proof (induct n)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc n)
+    assume hypa1:"length (a # ls) < Suc n"
+    then show ?case using hyp1 by (induct n , simp_all)
+  qed    
+qed
+    
+    
+    
+fun p03_2 :: "'a list \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a" where 
+  "p03_2 [] _ defa = defa"|
+  "p03_2 _ 0 defa = defa"|
+  "p03_2 (x#xs) (Suc 0) _ = x"|
+  "p03_2 (x#xs) (Suc n) defa = p03_2 xs n defa"
 
+lemma "n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> p03_2 ls n defa \<in> set ls"  by (induct ls n defa rule : p03_2.induct, simp_all)
+    
+lemma ""
+ 
+definition p03_3 :: "'a list \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a" where
+  "p03_3 ls n defa  = (let tmp = drop (n - 1) ls in if List.null tmp then defa else hd tmp)"
+
+lemma "n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> p03_3 ls n defa \<in> set ls" 
+proof (induct ls arbitrary : n)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons a ls)
+  assume hyp1:"\<And>n . n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> p03_3 ls n defa \<in> set ls"
+    and hyp2:"n \<le> length (a # ls)"
+    and hyp3:"n \<noteq> 0"
+  from hyp2 and hyp3  show ?case
+  proof (induct n)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc n)
+    then show ?case 
+  qed
+    
+qed
+  
+  
+primrec p03_4 :: 
+  
+fun p03_5 :: ""
+    (*
+fun bla :: "'a list \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a" where
+  "bla [] _ defa = defa"|
+  "bla _ 0 defa = defa"|
+  "bla (x#xs) (Suc 0) _ = x"|
+  "bla (x#xs) (Suc n) defa = bla xs n defa"
+  
+lemma "n \<le> length ls \<Longrightarrow> n \<noteq> 0 \<Longrightarrow> bla ls n defa \<in> set ls" proof (induct ls n defa rule : bla.induct)
+  case (1 uu defa)
+  then show ?case sorry
+next
+  case (2 v va defa)
+  then show ?case sorry
+next
+  case (3 x xs uw)
+  then show ?case sorry
+next
+  case (4 x xs v defa)
+  then show ?case sorry
+qed
+  *)
+  
+  
+  (*
+declare bla_def[simp]
+  thm bla_def
+lemma "bla (n::nat) = (0::nat) \<or> bla n = 2 \<or> bla n = 1" 
+  using [[simp_trace_new mode=full]]
+proof (induct n)
+  case 0
+    (*have "bla 0 = " *)
+  then show ?case by simp
+next
+  case (Suc n)
+  then show ?case sorry
+qed
+  *)
+    
+(*
 fun p03_1 :: "'a list \<Rightarrow> nat \<Rightarrow> 'a option" where
 "p03_1 [] _ = None"|
 "p03_1 (x#xs) n = (case n of  0 \<Rightarrow> None| (Suc 0) \<Rightarrow> Some x | Suc val  \<Rightarrow>  p03_1 xs val)"
@@ -135,4 +281,4 @@ qed
 lemma tmp : "length (val#ls) < n \<Longrightarrow> length ls < n" by auto
 
 lemma "(length ls < n \<Longrightarrow> P) \<Longrightarrow> length (a#ls) < (Suc n2) \<Longrightarrow> n = Suc n2 \<Longrightarrow> P" by simp
-    
+ *)   
